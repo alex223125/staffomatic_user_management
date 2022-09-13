fdd2e657ee3db5bb82bc60241e467d60e6950f3a
Alexey Sologub <alexeysologub51@gmail.com>
2022-09-13 19:58:07 -0400

    mailer for Operation entity added, feature of sending notifications added to users#update and destroy services

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,2 +1,11 @@
 class Operation < ApplicationRecord
+
+  after_commit :notify, on: :create
+
+  def notify
+    ActionNotificationMailer.with({operation: self})
+                            .action_notification_email
+                            .deliver_now
+  end
+
 end
---
+++ spec/mailers/action_notification_spec.rb
@@ -0,0 +1,5 @@
+require "rails_helper"
+
+RSpec.describe ActionNotificationMailer, type: :mailer do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
